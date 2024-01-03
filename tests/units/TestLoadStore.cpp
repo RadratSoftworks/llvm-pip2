61a068cf9a1ea28e3773f8e9c3c860699f0216a6
Đỗ Trọng Thư <lmt42@yandex.com>
2024-01-03 14:24:57 +0700

    tests: Add STW and STORE/RESTORE tests

--- tests/units/TestLoadStore.cpp
+++ tests/units/TestLoadStore.cpp
@@ -162,3 +162,141 @@ TEST_CASE("LDBUd: Load 8-bit unsigned to 32-bit register", "[PIP2][LoadStore][Si
 
     REQUIRE(env.reg(Register::P0) == static_cast<std::uint32_t>(randed));
 }
+
+TEST_CASE("STWd: Store 32-bit word", "[PIP2][LoadStore][Single]") {
+    ModifiablePoolItems pool_items;
+    RandomIntGenerator<std::uint32_t> rand = make_normal_constant_random_generator<std::uint32_t>();
+
+    std::vector<Instruction> instructions = {
+            make_unary_instruction(Opcode::STWd, Register::P1, Register::P0),
+            make_constant(12),
+            make_single_argument_instruction(Opcode::JPr, Register::RA)
+    };
+
+    TestEnvironment env("STWd", instructions, std::move(pool_items), 0, 20);
+
+    RandomIntGenerator<std::uint32_t> rand_32;
+
+    auto *heap = reinterpret_cast<std::uint32_t*>(env.heap());
+    const auto randed = rand_32.next();
+
+    env.reg(Register::P0, env.heap_address() + 4);
+    env.reg(Register::P1, randed);
+
+    env.run();
+
+    REQUIRE(heap[4] == randed);
+}
+
+TEST_CASE("STHd: Store 16-bit word", "[PIP2][LoadStore][Single]") {
+    ModifiablePoolItems pool_items;
+    RandomIntGenerator<std::uint32_t> rand = make_normal_constant_random_generator<std::uint32_t>();
+
+    std::vector<Instruction> instructions = {
+            make_unary_instruction(Opcode::STHd, Register::P1, Register::P0),
+            make_constant(12),
+            make_single_argument_instruction(Opcode::JPr, Register::RA)
+    };
+
+    TestEnvironment env("STHd", instructions, std::move(pool_items), 0, 20);
+
+    auto *heap = reinterpret_cast<std::uint16_t*>(env.heap());
+    auto randed = rand.next();
+
+    env.reg(Register::P0, env.heap_address() + 4);
+    env.reg(Register::P1, randed);
+
+    env.run();
+
+    REQUIRE(heap[8] == (randed & 0xFFFF));
+    REQUIRE(heap[9] == 0);
+}
+
+TEST_CASE("STBd: Store 8-bit word", "[PIP2][LoadStore][Single]") {
+    ModifiablePoolItems pool_items;
+    RandomIntGenerator<std::uint32_t> rand = make_normal_constant_random_generator<std::uint32_t>();
+
+    std::vector<Instruction> instructions = {
+            make_unary_instruction(Opcode::STBd, Register::P1, Register::P0),
+            make_constant(12),
+            make_single_argument_instruction(Opcode::JPr, Register::RA)
+    };
+
+    TestEnvironment env("STBd", instructions, std::move(pool_items), 0, 20);
+
+    auto *heap = reinterpret_cast<std::uint8_t*>(env.heap());
+    auto randed = rand.next();
+
+    env.reg(Register::P0, env.heap_address() + 4);
+    env.reg(Register::P1, randed);
+
+    env.run();
+
+    REQUIRE(heap[16] == (randed & 0xFF));
+    REQUIRE(heap[17] == 0);
+    REQUIRE(heap[18] == 0);
+    REQUIRE(heap[19] == 0);
+}
+
+TEST_CASE("STORE: Store registers to stack", "[PIP2][LoadStore][Single]") {
+    ModifiablePoolItems pool_items;
+
+    std::vector<Instruction> instructions = {
+            make_range_reg_instruction(Opcode::STORE, Register::P0, 16),
+            make_single_argument_instruction(Opcode::JPr, Register::RA)
+    };
+
+    TestEnvironment env("STORE", instructions, std::move(pool_items), 0, 20);
+
+    RandomIntGenerator<std::uint32_t> rand_32;
+    std::uint32_t value = rand_32.next();
+    std::uint32_t value2 = rand_32.next();
+    std::uint32_t value3 = rand_32.next();
+    std::uint32_t value4 = rand_32.next();
+
+    env.reg(Register::SP, env.heap_address() + 20);
+    env.reg(Register::P0, value);
+    env.reg(Register::P1, value2);
+    env.reg(Register::P2, value3);
+    env.reg(Register::P3, value4);
+
+    env.run();
+
+    auto *heap = reinterpret_cast<std::uint32_t*>(env.heap());
+
+    REQUIRE(heap[1] == value);
+    REQUIRE(heap[2] == value2);
+    REQUIRE(heap[3] == value3);
+    REQUIRE(heap[4] == value4);
+}
+
+TEST_CASE("RESTORE: Restore registers from stack", "[PIP2][LoadStore][Single]") {
+    ModifiablePoolItems pool_items;
+
+    std::vector<Instruction> instructions = {
+            make_range_reg_instruction(Opcode::RESTORE, Register::P3, 16),
+            make_single_argument_instruction(Opcode::JPr, Register::RA)
+    };
+
+    TestEnvironment env("RESTORE", instructions, std::move(pool_items), 0, 20);
+
+    RandomIntGenerator<std::uint32_t> rand_32;
+    std::uint32_t value = rand_32.next();
+    std::uint32_t value2 = rand_32.next();
+    std::uint32_t value3 = rand_32.next();
+    std::uint32_t value4 = rand_32.next();
+
+    auto *heap = reinterpret_cast<std::uint32_t*>(env.heap());
+    heap[1] = value;
+    heap[2] = value2;
+    heap[3] = value3;
+    heap[4] = value4;
+
+    env.reg(Register::SP, env.heap_address() + 4);
+    env.run();
+
+    REQUIRE(env.reg(Register::P0) == value);
+    REQUIRE(env.reg(Register::P1) == value2);
+    REQUIRE(env.reg(Register::P2) == value3);
+    REQUIRE(env.reg(Register::P3) == value4);
+}
